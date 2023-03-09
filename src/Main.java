3e44d58ed3b71d5d79c1d4268d77f1ae50bc27e2
Kasper-Loontjens <kasper.loontjens@gritacademy.se>
2023-03-09 14:06:57 +0100

    Skapa Json

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,10 @@ import org.json.simple.parser.ParseException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
@@ -25,8 +29,35 @@ public class Main {
 
         Object newO = new JSONParser().parse(new FileReader("src/jsonText.json"));
         JSONObject jsonData = (JSONObject) newO;
-        JSONObject person = (JSONObject) jsonData.get("p1");
-        String name= (String) person.get("name");
-        System.out.println("P1 Name :" + name);
+        JSONObject person1 = (JSONObject) jsonData.get("p1");
+        JSONObject person2 = (JSONObject) jsonData.get("p2");
+        String name = (String) person1.get("name");
+
+        int ageP1 = Integer.parseInt(person1.get("age").toString());
+        System.out.println("P1 Name: " + name + "--- P2 Name: "+ person2.get("name"));
+        System.out.println((name + " is " + ageP1 + " years old."));
+        System.out.println((person2.get("name") + " is " + person2.get("age") + " years old."));
+
+        fetchJsonFromApi();
+    }
+
+
+    static void fetchJsonFromApi() throws IOException, ParseException {
+        URL issUrl = new URL("https://api.wheretheiss.at/v1/satellites/25544");
+        HttpURLConnection conn = (HttpURLConnection) issUrl.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+        if (conn.getResponseCode() == 200) System.out.println("lyckat");
+        else System.out.println("miss");
+
+        StringBuilder strData = new StringBuilder();
+        Scanner scanner = new Scanner(issUrl.openStream());
+        while (scanner.hasNext()) {
+            strData.append(scanner.nextLine());
+        }
+        scanner.close();
+        JSONObject dataObject = (JSONObject) new JSONParser().parse(String.valueOf(strData));
+        System.out.println(dataObject);
+        System.out.println("Velooo: " +dataObject.get("velocity"));
     }
 }
