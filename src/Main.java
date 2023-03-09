e0379cebc9226c13281b044434c468efa72b842f
Kasper-Loontjens <kasper.loontjens@gritacademy.se>
2023-03-09 11:05:04 +0100

    Skapa Json

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 import netscape.javascript.JSObject;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException {
 
         System.out.println("Hello world!");
         System.out.println("Hello world!");
@@ -17,6 +23,10 @@ public class Main {
 
         System.out.println("Jag är " + jsonOb.get("age")+ " år gammal");
 
-
+        Object newO = new JSONParser().parse(new FileReader("src/jsonText.json"));
+        JSONObject jsonData = (JSONObject) newO;
+        JSONObject person = (JSONObject) jsonData.get("p1");
+        String name= (String) person.get("name");
+        System.out.println("P1 Name :" + name);
     }
 }
