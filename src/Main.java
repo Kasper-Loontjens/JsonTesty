f0138654649202320024f454b8182e614955dac7
Kasper-Loontjens <kasper.loontjens@gritacademy.se>
2023-03-09 10:27:24 +0100

    Skapa Json

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import netscape.javascript.JSObject;
+import org.json.simple.JSONObject;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Helloooooo");
+
+        JSONObjects jsonOb = new JSONObject();
+
+        jsonOb.put("namn", "marcus");
+        jsonOb.put("age", 34);
+
+        System.out.println("Mitt namn är: " + jsonOb.get("namn"));
+
+        System.out.println("Jag är " + jsonOb.get("age")+ " år gammal");
+
+
     }
 }
